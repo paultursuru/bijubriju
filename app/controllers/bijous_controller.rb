efc661f89847b9fa0eaa3aaa8874d9ac4123529c
paul lahana <paul.lahana@gmail.com>
2020-01-09 18:10:14 +0100

    adding and editing bijoux

--- app/controllers/bijous_controller.rb
+++ app/controllers/bijous_controller.rb
@@ -26,26 +26,41 @@ class BijousController < ApplicationController
   end
 
   def new
-
+    @bijou = Bijou.new
   end
 
   def create
+    @bijou = Bijou.new(bijou_params)
 
+    if @bijou.save
+      redirect_to bijou_path(@bijou)
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @bijou = Bijou.find(params[:id])
   end
 
   def update
-
+    @bijou = Bijou.find(params[:id])
+    if @bijou.update(bijou_params)
+      redirect_to bijou_path(@bijou)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @bijou.destroy
+    redirect_to bijous_path
   end
 
   private
 
+  def bijou_params
+    params.require(:bijou).permit(:sku, :name, :category, :price_cents)
+  end
 
 end
